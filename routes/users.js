1bfc8080462d93ba900969233b46f2cbf4f37842
Adalberto Maia <btowmaia@gmail.com>
2022-04-12 16:25:24 -0300

    last up

--- routes/users.js
+++ routes/users.js
@@ -23,6 +23,7 @@ module.exports = function(app){
   
   
   route.post((req, res) => {
+      if(!app.utils.validator.user(app, req, res)) return false;
       db.insert(req.body, (err, user)=>{
       if(err){
         app.utils.error.send(err, req, res);
@@ -31,6 +32,38 @@ module.exports = function(app){
       }
     })
   
-  })
+  });
+
+  let routeId = app.route('/users/:id');
+  routeId.get((req,res) => {
+    db.findOne({_id:req.params.id}).exec((err,user)=>{
+      if(err){
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user);
+      }
+    });
+  });
+
+  routeId.put((req,res) => {
+    if(!app.utils.validator.user(app, req, res)) return false;
+    db.update({_id:req.params.id},req.body, err =>{
+      if(err){
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(Object.assign(req.body,req.params));
+      }
+    });
+  });
+
+  routeId.delete((req,res)=>{
+    db.remove({_id: req.params.id}, {}, err=>{
+      if(err){
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(req.params);
+      }  
+    });
+  });
 }
 
